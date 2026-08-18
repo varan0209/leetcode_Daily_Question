5c099eac53e864eb783935baabd9ecc46613b1ec
varan0209 <94162125+varan0209@users.noreply.github.com>
2026-08-18 22:14:28 +0530

    Time: 0 ms (100%), Space: 19.2 MB (75.41%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,21 +1,16 @@
-from typing import List
-
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
-        count = {}
+        # k가 전체 길이와 같으면 무조건 최댓값
+        if k == len(nums):
+            return max(nums)
 
-        # Check every subarray of size k
-        for i in range(len(nums) - k + 1):
-            subarray = set(nums[i:i + k])
-
-            for x in subarray:
-                count[x] = count.get(x, 0) + 1
-
-        # Find the largest number appearing in exactly one subarray
-        ans = -1
-
-        for x in count:
-            if count[x] == 1:
-                ans = max(ans, x)
-
-        return ans
+        # k == 1이면 1번만 나온 숫자 중 최댓값
+        if k == 1:
+            arr = [x for x in nums if nums.count(x) == 1]
+        
+        # 위에 해당되지 않으면 nums[0], nums[-1]만 후보임
+        else:
+            arr = [x for x in (nums[0], nums[-1]) if nums.count(x) == 1]
+        
+        # arr 없으면 -1
+        return max(arr) if arr else -1
