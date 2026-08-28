5ba8a2a4dcf64f06ef0a5833320e13890493786a
varan0209 <94162125+varan0209@users.noreply.github.com>
2026-08-28 23:16:26 +0530

    Time: 11 ms (90.91%), Space: 19.5 MB (68.18%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.py
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.py
@@ -1,83 +1,27 @@
 class Solution:
     def lexPalindromicPermutation(self, s: str, target: str) -> str:
-        from collections import Counter
-
-        cnt = Counter(s)
-
-        # A palindrome can have at most one odd-frequency character
-        odd = [c for c in cnt if cnt[c] % 2]
-
-        if len(odd) > 1:
+        n, cnt = len(s), Counter(s)
+        odds = [c for c, v in cnt.items() if v % 2]
+        if len(odds) > n % 2:
             return ""
 
-        middle = odd[0] if odd else ""
+        mid, h = (odds[0] if odds else ""), n // 2
+        pool = Counter({c: v // 2 for c, v in cnt.items()})
+        build = lambda half: half + mid + half[::-1]
+        rest = lambda: "".join(c * pool[c] for c in sorted(pool))
 
-        # Count characters for the left half
-        half_count = [0] * 26
+        stop = 0                                     # longest prefix of target[:h] the pool can match
+        while stop < h and pool[target[stop]]:
+            pool[target[stop]] -= 1
+            stop += 1
 
-        for i in range(26):
-            ch = chr(97 + i)
-            half_count[i] = cnt[ch] // 2
+        if stop == h and (p := build(target[:h])) > target:
+            return p                                 # half forced -> exactly one candidate
 
-        m = len(s) // 2
-
-        def build(left):
-            left = ''.join(left)
-            return left + middle + left[::-1]
-
-        # Try to match target's first half
-        left = []
-        remaining = half_count[:]
-        mismatch = -1
-
-        for i in range(m):
-            t = ord(target[i]) - 97
-
-            if remaining[t] > 0:
-                left.append(chr(97 + t))
-                remaining[t] -= 1
-            else:
-                mismatch = i
-                break
-
-        # Case 1: We matched the entire first half
-        if mismatch == -1:
-            candidate = build(left)
-
-            if candidate > target:
-                return candidate
-
-            # Otherwise, we need to increase the half.
-            start = m - 1
-
-        # Case 2: We couldn't match target at position mismatch
-        else:
-            start = mismatch
-
-        # Find the rightmost position we can increase
-        for i in range(start, -1, -1):
-
-            # Characters used before position i
-            remaining = half_count[:]
-
-            for j in range(i):
-                remaining[ord(left[j]) - 97] -= 1
-
-            # We need a character greater than target[i]
-            target_char = ord(target[i]) - 97
-
-            for c in range(target_char + 1, 26):
-                if remaining[c] > 0:
-
-                    new_left = left[:i] + [chr(97 + c)]
-                    remaining[c] -= 1
-
-                    # Fill the rest with the smallest characters
-                    for x in range(26):
-                        new_left.extend(
-                            [chr(97 + x)] * remaining[x]
-                        )
-
-                    return build(new_left)
-
+        for i in range(stop, -1, -1):                # walk back to the last raisable position
+            if i < h and (c := min((x for x in pool if x > target[i] and pool[x]), default="")):
+                pool[c] -= 1
+                return build(target[:i] + c + rest())
+            if i:
+                pool[target[i - 1]] += 1             # un-consume, restoring the pool for i-1
         return ""
