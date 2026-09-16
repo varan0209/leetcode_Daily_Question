4f6585652dd75861724c8f6a8823a6f99950164b
varan0209 <94162125+varan0209@users.noreply.github.com>
2026-09-16 21:34:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (95.69%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,18 +1,9 @@
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
-        dp = [[0] * (k + 1) for _ in range(n + 1)]
-        for i in range(n + 1):
-            dp[i][0] = 1  # 0 segments: exactly one way (draw nothing)
-
-        for j in range(1, k + 1):
-            prefix = [0] * (n + 1)
-            prefix[0] = dp[0][j - 1]
-            for t in range(1, n + 1):
-                prefix[t] = (prefix[t - 1] + dp[t][j - 1]) % MOD
-
-            for i in range(1, n + 1):
-                s = (prefix[i - 1] - dp[0][j - 1]) % MOD  # sum_{q=1}^{i-1} dp[q][j-1]
-                dp[i][j] = (dp[i - 1][j] + s) % MOD
-
-        return dp[n][k] % MOD
+        # g0 + s0 + g1 + s1 + g2  => k+1 g's and k s's 
+        # all gaps sum to n-1 i.e g0 + s0 + g1 + s1 + g2 = n-1 where s >= 1 and g >= 0
+        # g0 + s0' + g1 + s1' + g2 = n-1 where s' >= 0
+        # g0 + s0 + g1 + s1 + g2 = n-1-k here k =2
+        # now total elements on left = k + k+1 = 2k+1
+        # applying formula n+k-1 C k-1 here we get => n-1-k + 2k+1 - 1 C 2k+1 -1 = n + k - 1 C 2k 
+        return math.comb(n+k-1,2*k)%(10**9+7)
