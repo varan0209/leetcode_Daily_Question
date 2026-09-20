67dd9a9a27a9ff5c1889f4ee0a5bdfd0043f3b20
varan0209 <94162125+varan0209@users.noreply.github.com>
2026-09-20 23:58:46 +0530

    Time: 8 ms (34.96%), Space: 19.2 MB (87.39%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,7 +1,14 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        total = 0
-        for i, ch in enumerate(s, start=1):
-            rev_pos = 26 - (ord(ch) - ord('a'))
-            total += rev_pos * i
-        return total
+        n = len(s)
+        total = 123 * n * (n + 1) // 2
+        
+        sub = 0
+        idx = 1
+        _ord = ord 
+        
+        for ch in s:
+            sub += _ord(ch) * idx
+            idx += 1
+            
+        return total - sub
