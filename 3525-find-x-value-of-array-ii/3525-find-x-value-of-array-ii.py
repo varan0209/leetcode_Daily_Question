af5e4387fc596d7dca9ae6e4117fa2b978f68710
varan0209 <94162125+varan0209@users.noreply.github.com>
2026-09-22 22:44:08 +0530

    Time: 4061 ms (90.2%), Space: 63.5 MB (76.47%) - LeetHub

--- 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.py
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.py
@@ -1,81 +1,102 @@
+from typing import List
+
 class Solution:
     def resultArray(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
         n = len(nums)
-        cur = nums[:]
-
-        tree_P = [0] * (4 * n)
-        tree_cnt = [None] * (4 * n)
-
-        def leaf_value(val):
-            r = val % k
-            cnt1 = [0] * k
-            cnt1[r] = 1
-            return r, cnt1
-
-        def combine(left, right):
-            if left is None:
-                return right
-            if right is None:
-                return left
-            PL, cnt1L = left
-            PR, cnt1R = right
-            shifted = [0] * k
-            for t in range(k):
-                c = cnt1R[t]
-                if c:
-                    shifted[(PL * t) % k] += c
-            combined_cnt1 = [cnt1L[b] + shifted[b] for b in range(k)]
-            combined_P = (PL * PR) % k
-            return combined_P, combined_cnt1
-
-        def build(node, l, r):
-            if l == r:
-                P, cnt1 = leaf_value(cur[l])
-                tree_P[node] = P
-                tree_cnt[node] = cnt1
-                return
-            mid = (l + r) // 2
-            build(2 * node, l, mid)
-            build(2 * node + 1, mid + 1, r)
-            pull(node)
-
-        def pull(node):
-            left = (tree_P[2 * node], tree_cnt[2 * node])
-            right = (tree_P[2 * node + 1], tree_cnt[2 * node + 1])
-            P, cnt1 = combine(left, right)
-            tree_P[node] = P
-            tree_cnt[node] = cnt1
-
-        def update(node, l, r, idx):
-            if l == r:
-                P, cnt1 = leaf_value(cur[idx])
-                tree_P[node] = P
-                tree_cnt[node] = cnt1
-                return
-            mid = (l + r) // 2
-            if idx <= mid:
-                update(2 * node, l, mid, idx)
-            else:
-                update(2 * node + 1, mid + 1, r, idx)
-            pull(node)
-
-        def query(node, l, r, ql, qr):
-            if qr < l or r < ql:
-                return None
-            if ql <= l and r <= qr:
-                return (tree_P[node], tree_cnt[node])
-            mid = (l + r) // 2
-            left = query(2 * node, l, mid, ql, qr)
-            right = query(2 * node + 1, mid + 1, r, ql, qr)
-            return combine(left, right)
-
-        build(1, 0, n - 1)
-
-        result = []
-        for idx, val, start, x in queries:
-            cur[idx] = val
-            update(1, 0, n - 1, idx)
-            P, cnt1 = query(1, 0, n - 1, start, n - 1)
-            result.append(cnt1[x])
-
-        return result
+        # Pad n to the next power of 2 for a simplified iterative segment tree
+        m = 1
+        while m < n:
+            m *= 2
+            
+        tree_prod = [1] * (2 * m)
+        tree_counts = [[0] * k for _ in range(2 * m)]
+        
+        # Initialize the leaves of the segment tree
+        for i in range(n):
+            val = nums[i] % k
+            tree_prod[m + i] = val
+            tree_counts[m + i][val] = 1
+            
+        # Build the initial segment tree
+        for i in range(m - 1, 0, -1):
+            L = 2 * i
+            R = 2 * i + 1
+            L_prod = tree_prod[L]
+            L_counts = tree_counts[L]
+            R_prod = tree_prod[R]
+            R_counts = tree_counts[R]
+            
+            tree_prod[i] = (L_prod * R_prod) % k
+            for v in range(k):
+                tree_counts[i][v] = L_counts[v]
+            for v in range(k):
+                if R_counts[v]:
+                    nv = (L_prod * v) % k
+                    tree_counts[i][nv] += R_counts[v]
+                    
+        ans = []
+        for idx, value, start, x in queries:
+            # 1. Point Update
+            node = m + idx
+            val = value % k
+            tree_prod[node] = val
+            for i in range(k):
+                tree_counts[node][i] = 0
+            tree_counts[node][val] = 1
+            
+            node //= 2
+            while node > 0:
+                L = 2 * node
+                R = 2 * node + 1
+                L_prod = tree_prod[L]
+                L_counts = tree_counts[L]
+                R_prod = tree_prod[R]
+                R_counts = tree_counts[R]
+                
+                tree_prod[node] = (L_prod * R_prod) % k
+                for v in range(k):
+                    tree_counts[node][v] = L_counts[v]
+                for v in range(k):
+                    if R_counts[v]:
+                        nv = (L_prod * v) % k
+                        tree_counts[node][nv] += R_counts[v]
+                node //= 2
+                
+            # 2. Range Query [start, n - 1]
+            l = m + start
+            r = m + n - 1
+            
+            left_nodes = []
+            right_nodes = []
+            
+            # Find the nodes covering the query interval
+            while l <= r:
+                if l % 2 == 1:
+                    left_nodes.append(l)
+                    l += 1
+                if r % 2 == 0:
+                    right_nodes.append(r)
+                    r -= 1
+                l //= 2
+                r //= 2
+                
+            # Combine the nodes left-to-right
+            curr_prod = 1
+            curr_counts = [0] * k
+            
+            for node in left_nodes + right_nodes[::-1]:
+                nxt_prod = (curr_prod * tree_prod[node]) % k
+                nxt_counts = list(curr_counts)
+                
+                # Multiply combined past prefix products with current node's prefixes
+                for v in range(k):
+                    if tree_counts[node][v]:
+                        nv = (curr_prod * v) % k
+                        nxt_counts[nv] += tree_counts[node][v]
+                        
+                curr_prod = nxt_prod
+                curr_counts = nxt_counts
+                
+            ans.append(curr_counts[x])
+            
+        return ans
